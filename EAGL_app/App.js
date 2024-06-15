3cbdaf17c7460d08e9ac1bde782c966dc93e4fd7
MrMVP <mvpawar2005@gmail.com>
2024-06-15 21:06:22 +0530

    Added Remote App

---
+++ EAGL_app/App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LoginPage from "./app/LoginPage";
+import LocationPage from "./app/LocationPage";
+
+export default function App() {
+  return <LoginPage />;
+}

83a04ee9110567085ba42e36bf0e7a73dd945285
harshitnitjsr <harshit26092004@gmail.com>
2024-06-15 17:27:27 +0530

    backend first commit

---
+++ server/models/goat.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GoatSchema = new Schema({
+  tag: { type: String, required: true },
+  dob: { type: Date },
+  weight: { type: Number, min: 0 },
+  breed: { type: String },
+  gender: { type: String, enum: ["Male", "Female", "Unknown"] },
+  color: { type: String },
+  vaccinations: [
+    {
+      date: { type: Date },
+      vaccId: { type: Schema.Types.ObjectId, ref: "Vaccination" },
+    },
+  ],
+  issueDate: { type: Date },
+  updatedWeight: { type: Number },
+  insurance: { type: Boolean },
+  isAlive: { type: Boolean },
+  diseases: [{ type: String }],
+  sellingPrice: { type: Number },
+  numberOfChildren: { type: Number },
+  children: [{ type: Schema.Types.ObjectId, ref: "Goat" }],
+  comments: { type: String },
+  beneficId: { type: Schema.Types.ObjectId, ref: "Benefic" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Goat", GoatSchema);
